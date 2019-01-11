38b1a32caa18c6e5f846192f00dc352530d220c5
fadi habeeb <fadi habeeb@DESKTOP-7ARVRFN>
2019-01-11 23:20:43 +0200

    AddBook + EditBook + ManageLibrary controllers

--- src/controllers/AddBookController.java
+++ src/controllers/AddBookController.java
@@ -1,78 +1,143 @@
 package controllers;
 
+import java.io.IOException;
+
+import entities.Book;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.InputMethodEvent;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class AddBookController {
 
-    @FXML
-    private TextField txtBookName;
+	@FXML
+	private TextField txtBookName;
 
-    @FXML
-    private TextField txtAuthor;
+	@FXML
+	private TextField txtAuthor;
 
-    @FXML
-    private TextField txtBookID;
+	@FXML
+	private TextField txtBookID;
 
-    @FXML
-    private TextField txtEdition;
+	@FXML
+	private TextField txtEdition;
 
-    @FXML
-    private TextField txtPrintYear;
+	@FXML
+	private TextArea txtTableOfContents;
 
-    @FXML
-    private TextField txtBookSubject;
+	@FXML
+	private TextField txtPrintYear;
 
-    @FXML
-    private ImageView imgBack;
+	@FXML
+	private TextField txtBookSubject;
 
-    @FXML
-    private Button btnAddBook;
+	@FXML
+	private ImageView imgBack;
 
-    @FXML
-    private Button btnClear;
+	@FXML
+	private Button btnAddBook;
 
-    @FXML
-    private TextField txtCatalog;
+	@FXML
+	private Button btnClear;
 
-    @FXML
-    private TextField txtCopies;
+	@FXML
+	private TextField txtCatalog;
 
-    @FXML
-    private TextField txtShelf;
+	@FXML
+	private TextField txtCopies;
 
-    @FXML
-    private TextArea txtDescirption;
+	@FXML
+	private TextField txtShelf;
 
-    @FXML
-    private Button btnBrowsePath;
+	@FXML
+	private TextArea txtDescirption;
 
-    @FXML
-    private TextField txtPath;
+	@FXML
+	private Button btnBrowsePath;
 
-    @FXML
-    void btnAddBookPressed(ActionEvent event) {
+	@FXML
+	private TextField txtPath;
 
-    }
+	@FXML
+	void btnAddBookPressed(ActionEvent event) throws IOException {
+		Book newBook = new Book(
+				Integer.parseInt(txtBookID.getText()), 
+				txtBookName.getText(),
+				txtAuthor.getText(),
+				Double.parseDouble(txtEdition.getText()),
+				Integer.parseInt(txtPrintYear.getText()),
+				txtBookSubject.getText(),
+				txtDescirption.getText(), 
+				Integer.parseInt(txtCatalog.getText()),
+				txtTableOfContents.getText(),
+				txtShelf.getText(), 
+				Integer.parseInt(txtCopies.getText()));
 
-    @FXML
-    void btnBrowsePathPressed(ActionEvent event) {
+		/*
+		 * needs DB try {
+		 * 
+		 * if(Integer.parseInt(txtBookID.getText()) is exist in db){ throw new
+		 * Exception(); } catch(Exception e) { Alert alert = new
+		 * Alert(AlertType.INFORMATION); alert.setTitle("Error");
+		 * alert.setHeaderText("this id is exist");
+		 * alert.setContentText("please enter a new id "); alert.showAndWait(); } }
+		 */
 
-    }
+		// write to this book to DB
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("../gui/ManageLibraryForm.fxml"));
+		Stage stage = new Stage();
+		stage.setScene(new Scene((Parent) loader.load()));
+		stage.show();
+		((Node) event.getSource()).getScene().getWindow().hide();
 
-    @FXML
-    void btnClearPressed(ActionEvent event) {
+	}
 
-    }
+	@FXML
+	void btnBrowsePathPressed(ActionEvent event) {
 
-    @FXML
-    void imgBackClicked(MouseEvent event) {
+	}
 
-    }
+	@FXML
+	void btnClearPressed(ActionEvent event) {	
+		txtBookName .clear();
+	}
+
+	@FXML
+	void imgBackClicked(MouseEvent event) throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("../gui/ManageLibraryForm.fxml"));
+		Stage stage = new Stage();
+		stage.setScene(new Scene((Parent) loader.load()));
+		stage.show();
+		((Node) event.getSource()).getScene().getWindow().hide();
+	}
+
+	// disable the button until fill all the data
+	/*
+	 * @FXML void EnableAddBookButton(InputMethodEvent event) {
+	 * System.out.println("dddd");
+	 * if(txtBookName.getText()==null||txtAuthor.getText()==null||txtBookID.getText(
+	 * )==null||txtEdition.getText()==null||txtPrintYear.getText()==null||
+	 * txtBookSubject.getText()==null||txtCatalog.getText()==null||txtCopies.getText
+	 * ()==null||txtShelf.getText()==null) btnAddBook.setDisable(true); else
+	 * btnAddBook.setDisable(false); }
+	 */
+
+	@FXML
+	void initialize() {
+		btnAddBook.setDisable(true);
+
+	}
 
 }
--- src/controllers/EditBookController.java
+++ src/controllers/EditBookController.java
@@ -1,23 +1,25 @@
 package controllers;
 
-import java.awt.TextField;
-import java.net.URL;
-import java.util.ResourceBundle;
+import java.io.IOException;
+import java.sql.Date;
 
+import entities.Book;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class EditBookController {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
+	
+	Book editedBook;
 
     @FXML
     private TextField txtBookName;
@@ -63,22 +65,63 @@ public class EditBookController {
 
     @FXML
     void btnBrowsePathPressed(ActionEvent event) {
-
+    	//adam will make it 
     }
 
     @FXML
     void btnEditBookPressed(ActionEvent event) {
-
+ /*
+  * update the edited data in the DB    	
+  */
+    	editedBook.setName(txtBookName.getText());
+    	editedBook.setAuthor(txtAuthor.getText());
+    	editedBook.setBookID(Integer.parseInt(txtBookID.getText()));
+    	editedBook.setEdition(Double.parseDouble(txtEdition.getText()));
+    	editedBook.setPrintYear(Integer.parseInt(txtPrintYear.getText()));
+    	editedBook.setSubject(txtSubject.getText());
+    	editedBook.setCatalog(Integer.parseInt(txtCatalog.getText()));
+    	editedBook.setCopiesNumber(Integer.parseInt(txtCopies.getText()));
+    	editedBook.setShelf(txtShelf.getText());
+    	//Updated edited book in DB
+    	//add success message 
     }
 
+    
     @FXML
-    void imgBackClicked(MouseEvent event) {
+    void imgBackClicked(MouseEvent event) throws IOException {
+    	/*
+    	 * back to the previous screen
+    	 */
+    	    FXMLLoader loader = new FXMLLoader(getClass().getResource("../gui/ManageLibraryForm.fxml"));
+    	    Stage stage = new Stage();
+    	    stage.setScene(new Scene((Parent) loader.load()));
+    	    stage.show();
+    	    ((Node) event.getSource()).getScene().getWindow().hide();
 
     }
 
     @FXML
     void initialize() {
+   
 
+    }
+    
+    /*
+     * this function initialize the fields in the new window previous window selected book  
+     */
+    public void initFields(Book selectedBook) {
+    	
+    	editedBook = selectedBook; 
+    	txtBookName.setText(selectedBook.getName());	
+    	txtAuthor.setText(selectedBook.getAuthor());
+    	txtBookID.setText(Integer.toString(selectedBook.getBookID()));
+    	txtEdition.setText(Double.toString(selectedBook.getEdition()));
+    	//txtPrintYear.setText(Date.selectedBook.getPrintYear());
+    	txtSubject.setText(selectedBook.getSubject());
+    	txtCatalog.setText(Integer.toString(selectedBook.getCatalog()));
+    	txtCopies.setText(Integer.toString(selectedBook.getCopiesNumber()));
+    	txtShelf.setText(selectedBook.getShelf());
+    	
     }
 }
 
