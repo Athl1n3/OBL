868a52136424815021f2a0687f22e5fb1f7e9e06
Adam Mahameed <adam.spark199@gmail.com>
2019-02-01 23:40:17 +0200

    Properties files added

---
+++ src/controllers/MainLauncher.java
@@ -0,0 +1,40 @@
+package controllers;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import client.ClientConnection;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * OBL System 2018-2019
+ * 
+ * Credits:
+ * -Adam Mahameed [GUI Design, LibraryServices, MySQLConnection, ArchivedDataController, ActivityController,
+ * 					 AccountDetailsController, SceneController, UserLookupController, NotificationsController, Main forms controllers]
+ * -Saleh Qassem [DatabaseController, ReturnController, LoginController, SearchController, MySQLConnection, OrderController]
+ * -Fadi Habeeb [AddBookController, EditBookController, ManageLibraryController]
+ * -Alaa Grable [NewAccountController, ExtendLendController, ManualExtendController, LendController, ReportsController]
+ * -Ahmad Qais [AccountDetailsController]
+ */
+public class MainLauncher extends Application {
+	public static void main(String[] args) { // TODO Auto-generated method stub
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Properties props = new Properties();
+		FileInputStream in = new FileInputStream("@/../Client.properties");
+		props.load(in);
+		in.close();
+		String host = props.getProperty("server.host");
+		int port = Integer.parseInt(props.getProperty("server.port"));
+		DatabaseController.InitiateClient(new ClientConnection(host,port));
+		primaryStage.setOnCloseRequest(event -> DatabaseController.terminateClient());
+		
+		MainFormController aFrame = new MainFormController();
+		aFrame.start(primaryStage);
+	}
+}
